a817a8c36a2d833f66fc9310010af31d0d99e30b
srocha <srocha@rochabook.(none)>
2014-12-05 17:48:11 -0300

    Agregando más modulos y ordenando un poco codigo

--- dirdoc.py
+++ dirdoc.py
@@ -1,14 +1,14 @@
-
+# -*- coding: utf-8 -*-
 """
 Created on Thu Dec  4 22:49:37 2014
 
 @author: srocha
 """
-
 import requests
 from bs4 import BeautifulSoup
 import re
 import logging
+import urlparse
 
 # create logger
 logger = logging.getLogger('Dirdoc API')
@@ -27,55 +27,65 @@ ch.setFormatter(formatter)
 # add ch to logger
 logger.addHandler(ch)
 
-def requestpost(url,data,headers):
-    write = "Post URL = {0}".format(url)
-    logger.info(write)
-    req = requests.post(url,data=data,headers=headers)
-    return req
-    
-    
-def requestget(url,cookies,headers):
-    write = "Get URL = {0}".format(url)
-    logger.info(write)
-    req = requests.post(url,cookies=cookies,headers=headers)
-    return req
-
 class Dirdoc():
+
+    def __requestpost(self,url):
+        write = "Post URL = {0}".format(url)
+        logger.info(write)
+        req = requests.post(url,data=self.__logindata,headers=self.__headers)
+        self.__cookies = req.cookies
+        html = BeautifulSoup(req.text)
+        return html
+
+
+    def __requestget(self,url):
+        write = "Get URL = {0}".format(url)
+        logger.info(write)
+        req = requests.get(url,cookies=self.__cookies,headers=self.__headers)
+        html = BeautifulSoup(req.text)
+        return html
     
     def __login(self):
         url = self.__loginurl
-        data = self.__logindata
-        headers = self.__headers
-        req = requestpost(url,data,headers)
-        if "Bienvenido" not in req.text:
+        html = self.__requestpost(url)
+        if "Bienvenido" not in html.text:
             logger.error("al realizar Login, password y/o rut")
-            #raise LoginException("Error al hacer login")
             login = False
         else:
             logger.info("Login Correcto!")
-            self.__cookies = req.cookies
             login = True
         return login
         
     def __repr__(self):
         return "API Dirdoc rut = {0}".format(self.rut)
         
-    def __getInfo(self,html):
+    def __getInfo(self):
+        html = self.__htmls['info']
         info = html.select("html body table.pequena tr.centro td")
         rut = info[0].get_text()
         nombre = info[1].text
         estado = info[2].text
         carrera = info[3].text
         return (rut,nombre,estado,carrera)
+        
+    def __getIdramo(self,ramo):
+        id_ramo = ramo[0].text#.encode(self.__encoding)
+        write = "id_ramo = {0}".format(id_ramo)
+        #logger.debug(write)
+        if len(unicode(id_ramo.split('\n'))) > 0:
+            id_ramo.replace('\n','').replace('\r','')
+        return id_ramo
     
     def __getRamos(self,html):
         reg_exp_link = r'(?<=p2=)\d+'
         ramos = html.select("html body table.pequena tr")[3:]
         todos_ramos = []
+        write = "Ramos = {0}".format(ramos)
+        #logger.debug(write)
         for ramo in ramos:
             ramo = ramo.select("td")
             info_ramo = {}
-            info_ramo['id_ramo'] = ramo[0].text.encode(self.__encoding) if ramo[0].text.encode(self.__encoding) is not u'' else None
+            info_ramo['id_ramo'] = self.__getIdramo(ramo)
             info_ramo['nombre'] = ramo[1].text if ramo[1].text is not u'' else None
             info_ramo['profesor'] = ramo[2].text if ramo[2].text is not u'' else None
             info_ramo['seccion ']= int(ramo[3].text) if ramo[3].text is not u'' else None
@@ -92,20 +102,71 @@ class Dirdoc():
 
     def __getRamo(self,id):
         url = self.__link_nota.format(id)
-        cookies = self.__cookies
-        headers = self.__headers
-        req = requestget(url,cookies,headers)
-        html = BeautifulSoup(req.text)
+        html = self.__requestget(url)
         tables = html.select("html body table.pequena tr")[4:]
         notas = []
         for por,nota in zip(tables[0].select("th"),tables[1].select("th")):
             por = int(re.findall(r'\d+', por.text)[0]) if len(re.findall(r'\d+', por.text)) else None
-            nota = float(''.join(nota.text.split(",")[:1])) if len(re.findall(r'\d.\d+', nota.text)) else None
+            nota = float(''.join(nota.text.split(",")[:1])) if len(re.findall(r'\d,\d+', nota.text)) else None
             notas.append({"nota":nota,"porcentaje":por})
         return notas
+
+    def __getLinkCarrera(self,datos):
+        link_carrera = datos.a.get("href")
+        query = urlparse.urlparse(link_carrera)[4]
+        #logger.debug("link_ramo = {0}".format(query))
+        return query
+
+    def __getPlanCarrera(self,malla):
+        plan = None
+        #plan = int(malla.select("html body table.pequena tr")[5].select("td")[1].text)
+        return plan
+
+    def __getMalla(self,link_malla):
+        url = self.__link_malla.format(link_malla)
+        html = self.__requestget(url)
+        #logger.debug("html = {0}".format(html))
+        avance = {}
+        avance['plan'] = self.__getPlanCarrera(html)
+        #logger.debug("info table = {0}".format(html.select("html body")))
+
+        return avance
+    
         
-        
-        
+    def __getCarreras(self):
+        html = self.__htmls['carreras']
+        info_table = html.select("html body table.pequena")[1].select("tr")[0].select("th")#info de la tabla
+        carreras = html.select("html body table.pequena")[1].select("tr")[1:]#todas las carreras
+        #logger.debug("info_table = {0}".format(info_table))
+        #logger.debug("carreras = {0}".format(carreras))
+        carreras_parse = []
+        for carrera in carreras:
+            carrera = carrera.select("td")
+            carrera_parse = {}
+            for (i,info) in enumerate(info_table):
+                dato = carrera[i]
+                if i is 0:
+                    link_malla = self.__getLinkCarrera(dato)
+                    carrera_parse['link'] = link_malla
+                    carrera_parse['avance'] = self.__getMalla(link_malla)
+                carrera_parse[info.text] = carrera[i].text
+            carreras_parse.append(carrera_parse)
+        return carreras_parse
+
+    def __getResultadoIns(self):
+        html = self.__htmls['resultado']
+        info_table = [i.text for i in html.select("html body table.pequena")[1].select("tr.titulo_fila th")]
+        ramos = html.select("html body table.pequena")[1].select("tr.centro")
+        #logger.debug("info_table ={0}".format(info_table))
+        #logger.debug("ramos = {0}".format(ramos))
+        resultados = []
+        for ramo in ramos:
+            res = {}
+            dat = [i.text for i in ramo.select("td")]
+            for (i,info) in enumerate(info_table):
+                res[info] = dat[i]
+            resultados.append(res)
+        return resultados
     
     def __init__(self,rut,password,cache = 100):
         self.__loginurl = "http://postulacion.utem.cl/valida.php"
@@ -119,8 +180,11 @@ class Dirdoc():
             notas = 'http://postulacion.utem.cl/alumnos/acta.php', # URL donde se ven las notas, recibe el link del ramo como parametro
             info = 'http://postulacion.utem.cl/inscripcion/horario',
             ramos_ant = "http://postulacion.utem.cl/curricular/notas_anterior",
+            carreras = 'http://postulacion.utem.cl/curricular',
+            resultado = 'http://postulacion.utem.cl/inscripcion/resultado',
         )
         self.__link_nota = "http://postulacion.utem.cl/curricular/notas/{0}"
+        self.__link_malla = "http://postulacion.utem.cl/alumnos/avance_malla_ci.php?{0}"#esta url ya no se usa pero me alimento de ella
         self.__htmls = {}
         self.__encoding = 'utf-8'
         self.__logindata = dict(
@@ -132,14 +196,16 @@ class Dirdoc():
         if self.__login():
             logger.info("Obteniendo informacion")
             
-            for url in self.__urls.items():
-                req = requestget(url[1],self.__cookies,self.__headers)
-                self.__htmls[url[0]] = BeautifulSoup(req.text)
+            for tupl in self.__urls.items():
+                key,url = tupl[0],tupl[1]
+                self.__htmls[key] = self.__requestget(url)
             
 
-            self.info['info'] = self.__getInfo(self.__htmls['info'])
+            self.info['info'] = self.__getInfo()
             self.info['ramos_actual'] = self.__getRamos(self.__htmls['ramos_actual'])
             self.info['ramos_ant'] = self.__getRamos(self.__htmls['ramos_ant'])
+            self.info['carreras'] = self.__getCarreras()
+            self.info['resultado'] = self.__getResultadoIns()
                 
         else:
             pass
